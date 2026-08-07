39d0b0c853f4d81c5c76b0ce601f049c4ea6f2a8
dhruvcode84-cell <dhruv.code.84@gmail.com>
2026-08-07 11:26:14 +0530

    Update ar-scene.js

--- js/ar-scene.js
+++ js/ar-scene.js
@@ -71,9 +71,9 @@ const SCENE_LAYOUT = {
   //   scale: 0.92
   // },
   fighterJet: {
-    position: { x: -1.25, y: 1.17, z: -0.24 },
+    position: { x: -1.25, y: 1.19, z: -0.24 },
     rotation: { x: 0, y: -180, z: -90 },
-    scale: 1.1,
+    scale: 1.16,
     flight: {
       startX: -1.25,
       endX: 1.25,
